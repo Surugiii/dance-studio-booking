8bc227c3ba25d69e4ea27d0b980a35790c1196c5
Surugiii <carlstzmejela@gmail.com>
2025-07-29 21:30:07 +0800

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("bookingForm").addEventListener("submit", function(event) {
-  event.preventDefault();
-  document.getElementById("confirmation").textContent = "Your booking request has been received!";
-  this.reset();
-});
